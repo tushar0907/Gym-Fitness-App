beb259613389cab75cf3feefae5208a7b46252d9
Elite Programmer <tusharkumar0807@gmail.com>
2022-11-22 12:02:41 +0530

    final changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -17,7 +17,7 @@ const Navbar=() => {
                         fontSize="24px"
                         alignItems="flex-end"
                   >
-                        <Link to="/" style={{textDecoration: 'none', color: '#3A1212', borderBottom: '3px solid #FF2625'}}>Home</Link>
+                        <Link to="/" style={{textDecoration: 'none', color: '#3A1212', borderBottom: '3px solid #2f3131'}}>Home</Link>
                         <a href="#exercises" style={{textDecoration: 'none', color: '#3A1212'}}>Exercises</a>
                   </Stack>
             </Stack>
