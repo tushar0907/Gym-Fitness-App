6573fa797605df1a221d6f482a60149ced36033a
Elite Programmer <tusharkumar0807@gmail.com>
2022-11-22 12:38:40 +0530

    logo and name change

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar=() => {
       return (
             <Stack direction="row" justifyContent="space-around" sx={{gap: {sm: '123px', xs: '40px'}, mt: {sm: '32px', xs: '20px'}, justifyContent: 'none'}} px="20px">
                   <Link to="/">
-                        <img src={Logo} alt="logo" style={{width: '48px', height: '48px', margin: '0px 20px'}} />
+                        <img src={Logo} alt="logo" style={{width: '72px', height: '68px', margin: '0px 20px'}} />
                   </Link>
                   <Stack
                         direction="row"
